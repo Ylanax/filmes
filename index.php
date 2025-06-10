0add59dd59009031bab01c4926c51ae5bda2eaa7
Ylana Pereira <ylana.pereira2004@outlook.com>
2025-06-09 22:24:08 -0300

    edição bootstrap card e col

--- index.php
+++ index.php
@@ -4,27 +4,49 @@ include "banner.php";
 ?>
 <div class="container">
     <h2 class="display-5">Melhores avaliações</h2>
-    <div class="row">
-        <div class="col">
-            <img src="img/filme1.webp">
+    <div class="row mt-5">
+        <div class="col-3">
+            <img src="img/filme1.webp" class="img-fluid">
             <h3>Jurassic Park</h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="col">
-            <img src="img/filme2.jpg">
+        <div class="col-3">
+            <img src="img/filme2.jpg" class="img-fluid">
             <h3>Karate Kid Legends</h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="col">
-            <img src="img/filme3.jpg">
+        <div class="col-3">
+            <img src="img/filme3.jpg" class="img-fluid">
             <h3>Lilo e Stitch</h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="col">
-            <img src="img/filme4.jpg">
+        <div class="col-3">
+            <img src="img/filme4.jpg" class="img-fluid">
             <h3>Pecadores</h3>
             <span>⭐ 10/10</span>
         </div>
     </div>
-</div>    
-    <?php include "rodape.php"; ?>
+
+    <div class="row">
+        <div class="col-6">
+            <img src="img/thunderbolts2.avif" class="img-fluid">
+        </div>
+        <div class="col-6 align-content-center">
+            <p>"Em Thunderbolts, a Marvel Studios reuniu uma equipe não convencional de anti-heróis – Yelena Belova, Bucky Barnes, Guardião Vermelho, Fantasma, Treinador e John Walker. Presos em uma armadilha mortal, sete párias desiludidos são forçados a embarcar em uma missão perigosa que os obrigará a confrontar os cantos mais sombrios de seus passados".</p>
+            <a href="https://www.youtube.com/watch?v=18qDST2_Nh8" class="btn btn-success
+            btn-lg">ASSISTA AO TRAILER AGORA</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6 align-content-center">
+            <p>O filme Hereditário (2018) conta a história da família Graham, que lida com a morte de sua matriarca, Ellen. Após sua morte, Annie (Toni Collette), seu marido e filhos começam a desvendar segredos perturbadores sobre sua ancestralidade e enfrentam eventos sobrenaturais que ameaçam sua sanidade e união familiar. O filme explora temas de luto, trauma e a inevitabilidade do destino.</p>
+            <a href="https://www.youtube.com/watch?v=Ui13PlmyZhQ" class="btn btn-success
+            btn-lg">ASSISTA AO TRAILER AGORA</a>
+        </div>
+        <div class="col-6">
+            <img src="img/hereditarioFilme.webp" class="img-fluid">
+        </div>
+    </div>
+</div>
+<?php include "rodape.php"; ?>
--- listadefilmes.php
+++ listadefilmes.php
@@ -1,57 +1,110 @@
 <?php
 include "cabecalho.php";
 ?>
-<h2>Todos os filmes</h2>
-<div class="lista-filmes">
-    <div class="card-filme">
-        <img src="img/filme1.webp">
-        <a href="umfilme.php"><h3>Jurassic Park</h3></a>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme1.webp">
-        <a href="umfilme.php"><h3>Jurassic Park</h3></a>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme1.webp">
-        <a href="umfilme.php"><h3>Jurassic Park</h3></a>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme1.webp">
-        <h3>Jurassic Park</h3>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme1.webp">
-        <h3>Jurassic Park</h3>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme1.webp">
-        <h3>Jurassic Park</h3>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme1.webp">
-        <h3>Jurassic Park</h3>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme2.jpg">
-        <h3>Karate Kid Legends</h3>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme3.jpg">
-        <h3>Lilo e Stitch</h3>
-        <span>⭐ 10/10</span>
-    </div>
-    <div class="card-filme">
-        <img src="img/filme4.jpg">
-        <h3>Pecadores</h3>
-        <span>⭐ 10/10</span>
+
+<div class="container">
+    <h2>Todos os filmes</h2>
+    <div class="row">
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.webp" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Karate Kid Legends</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme3.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Lilo e Stitch</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme4.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Pecadores</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?php include "rodape.php"; ?>
